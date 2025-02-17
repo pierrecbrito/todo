bc81867e56d3aaa8f3ac1c4cdbdc0ad6c8d329d3
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-17 19:54:10 -0300

    Footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import styles from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
         <p>
             <span>React + TS Todo</span> @2025
         </p>
