ce6de3590128d79c45b7dbedc14d96ba19ab5a60
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-17 19:42:11 -0300

    Header & Footer Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>TODO REACT</h1>
-    </div>
-  );
+  return <div>
+    <Header />
+    <h1>Conteúdo...</h1>
+   <Footer />
+  </div>
+  
 }
 
 export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>
+            <span>React + TS Todo</span> @2025
+        </p>
+  </footer>
+  )
+}
+
+export default Footer
