a5670a45f13d78ed058165740ad21344acc32024
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2025-02-18 21:25:03 -0300

    Task Style

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,9 +14,17 @@ const TaskList = ({taskList}: Props) => {
     <>
       {taskList.length > 0 ? (
         taskList.map((task) => (
-          <div key={task.id}>
-            <p>{task.title}</p>
+          <div key={task.id} className={styles.task}>
+            <div className={styles.task_info}>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div className={styles.task_actions}>
+              <i className='bi bi-pencil'></i>
+              <i className='bi bi-trash'></i>
+            </div>
           </div>
+          
         ))) : (<p>Não tem tarefas cadastradas</p>)}
     </>
   )
